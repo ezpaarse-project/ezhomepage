1e0378d087a19f4e9975d86e256620b15453a55f
wilmouths <wilmouth.steven@gmail.com>
2019-06-25 14:08:07 +0200

    update (localization, design, bibliomap live)

---
+++ assets/js/bibliomap.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function () {
+  axios.get(`http://bibliomap.inist.fr/`)
+  .then(function (response) {
+    if (response && response.status !== 200) {
+      hideBibliomapLive();
+      displayBibliomapOffline();
+    }
+
+    hideBibliomapOffline();
+    displayBibliomapLive();
+  })
+  .catch (function (error) {
+    hideBibliomapLive();
+    displayBibliomapOffline();
+  });
+});
+
+function displayBibliomapLive () {
+  $('#bibliomapLive').removeClass('hidden');
+  $('#exampleLive').removeClass('hidden');
+}
+
+function hideBibliomapLive () {
+  $('#bibliomapLive').addClass('hidden');
+  $('#exampleLive').addClass('hidden');
+}
+
+function displayBibliomapOffline () {
+  $('#bibliomapOff').removeClass('hidden');
+  $('#example').removeClass('hidden');
+}
+
+function hideBibliomapOffline () {
+  $('#bibliomapOff').addClass('hidden');
+  $('#example').addClass('hidden');
+}
