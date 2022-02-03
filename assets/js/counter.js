47ad0f54f31d5dde66499fd3a71c5a77e48e7324
wilmouths <wilmouthsteven@gmail.com>
2022-02-03 12:10:09 +0100

    pdate metrics display, and update js libs

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,64 +1,97 @@
 function counter () {
-  var nodes = {
-    indices: document.getElementById('counter3') || 78,
-    days: document.getElementById('counter2') || 365,
-    docs: document.getElementById('counter1') || 646060453,
-    platforms: document.getElementById('counter5') || 245,
-    titles: document.getElementById('counter4') || 161908,
-    contributors: document.getElementById('counter6'),
-    issues: document.getElementById('counter7')
+  let nodes = {
+    docs: document.getElementById('counter1'),
+    days: document.getElementById('counter2'),
+    indices: document.getElementById('counter3'),
+    titles: document.getElementById('counter4'),
+    platforms: document.getElementById('counter5'),
+    // badgesContributors: document.getElementById('counter6'),
+    // badgesIssues: document.getElementById('counter7'),
   };
+
+  const lang = document.querySelector('html').getAttribute('lang');
+  console.log(lang);
 
   function startCounter(metrics) {
     setTimeout(function () {
-      for (var p in nodes) {
+      for (let p in nodes) {
         if (metrics[p]) {
-          var counter = new CountUp(nodes[p], 0, metrics[p] || 0, 0, 3);
+          let counter = new countUp.CountUp(nodes[p], metrics[p] || 0, {
+            startVal: 0,
+            duration: 3,
+            separator: lang === 'fr' ? ' ' : ',',
+            decimal: lang === 'fr' ? ',' : '.',
+          });
           counter.start();
         }
       }
     }, 500);
   }
 
-  axios.get('https://ezmesure.couperin.org/api/metrics')
-    .then(function (response) {
-      if (response.status !== 200) {
-        return console.error('Failed to fetch ezMESURE metrics');
-      }
+  let currentYear = new Date().getFullYear();
 
-      var metrics = response.data.metrics;
-      metrics.docs = response.data.docs;
+  let defaultMetrics = {
+    docs: nodes.docs.getAttribute('data-nb'),
+    dateCoverage: {
+      min: new Date(`${currentYear}-01-01`),
+      max: new Date(`${currentYear}-12-31`),
+    },
+    days: nodes.days.getAttribute('data-nb'),
+    titles: nodes.titles.getAttribute('data-nb'),
+    platforms: nodes.platforms.getAttribute('data-nb'),
+    indices: nodes.indices.getAttribute('data-nb'),
+    // badgesIssues: nodes.badgesIssues.getAttribute('data-nb'),
+    // badgesContributors: nodes.badgesContributors.getAttribute('data-nb'),
+  };
 
-      axios.get('https://ezmesure.couperin.org/api/partners')
-      .then(function (response) {
-        if (response.status !== 200) {
-          return console.error('Failed to fetch ezMESURE metrics');
-        }
-        metrics.indices = response.data.length;
+  let ezmesure = axios.create({
+    baseURL: 'https://ezmesure.couperin.org/api',
+    timeout: 3000,
+  });
 
-        // axios.get('http://analyses.ezpaarse.org/api/badges/metrics')
-        // .then(function (response) {
-        //   if (response.status !== 200) {
-        //     return console.error('Failed to fetch ezpaarse-badge metrics');
-        //   }
+  const metricsData = async function() {
+    let partners;
+    try {
+      partners = await ezmesure.get('/partners');
+    } catch (err) {
+      console.error('Failed to fetch ezMESURE partners');
+      console.error(err);
+    }
 
-        //   var badgesData = response.data.data
-        //   var issues = 0;
-        //   for (var d in badgesData.metrics) {
-        //     issues = (issues + badgesData.metrics[d].issues.app)
-        //   }
-        //   metrics.issues = issues
-        //   metrics.contributors = badgesData.contributors
+    let ezmesureMetrics;
+    try {
+      ezmesureMetrics = await ezmesure.get('/metrics');
+    } catch (err) {
+      console.error('Failed to fetch ezMESURE metrics');
+      console.error(err);
+    }
 
-        //   startCounter(metrics);
-        // })
-        // .catch (function (error) {
-        //   console.error(error);
-        //   startCounter(metrics);
-        // });
+    // let badgesIssues;
+    // let badgesContributors;
+    // try {
+    //   const { data } = await axios.get('http://analyses.ezpaarse.org/api/badges/metrics');
+    //   let badgesData = data.data
+    //   for (let d in badgesData.metrics) {
+    //     badgesIssues = (badgesIssues + badgesData.metrics[d].issues.app)
+    //   }
+    //   badgesContributors = badgesData.contributors;
+    // } catch (err) {
+    //   console.error('Failed to fetch ezpaarse-badge metrics');
+    //   console.error(err);
+    // }
 
-        startCounter(metrics);
-      });
-    })
-    .catch (console.error);
+    return Promise.resolve({
+      indices: partners?.data?.length || defaultMetrics.indices,
+      docs: ezmesureMetrics?.data?.docs || defaultMetrics.docs,
+      days: ezmesureMetrics?.data?.metrics.days || defaultMetrics.days,
+      titles: ezmesureMetrics?.data?.metrics.titles || defaultMetrics.titles,
+      platforms: ezmesureMetrics?.data?.metrics.platforms || defaultMetrics.platforms,
+      // badgesIssues,
+      // badgesContributors,
+    });
+  };
+
+  metricsData()
+    .then((res) => startCounter({ ...defaultMetrics, ...res }))
+    .catch((err) => startCounter(defaultMetrics));
 }
