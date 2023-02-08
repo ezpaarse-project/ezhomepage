ec3942ee81037008da57993a1eb3cde431dedf2d
felixleo22 <leofelixpro22@gmail.com>
2023-02-08 11:18:19 +0100

    refactor: use v-for

--- src/components/skeleton/Drawer.vue
+++ src/components/skeleton/Drawer.vue
@@ -18,99 +18,45 @@
         </v-list-item-content>
       </v-list-item>
 
-      <v-list-item link :href="analogistURL">
-        <img
-          style="max-width: 30px"
-          :src="require('../../../public/images/analogist-logo.png')"
-          class="mr-6"
-          alt="analogist-img"
-        />
-
-        <v-list-item-content>
-          <v-list-item-title> Analogist </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-list-item link :href="ezpaarseDemoURL">
-        <v-list-item-icon>
-          <v-icon>mdi-chevron-double-right</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title v-text="$t('demonstration')"> </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-list-item link :href="ezmesureURL">
-        <img
-          style="max-width: 32px"
-          :src="require('../../../public/images/ezmesure-logo.svg')"
-          class="mr-6"
-          alt="ezmesure-img"
-        />
-
-        <v-list-item-content>
-          <v-list-item-title> Ezmesure </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-list-item link :href="bibliomapURL">
-        <v-list-item-icon>
-          <v-icon>mdi-web</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title> bibliomap </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
       <v-list-item link :href="ezpaarseDocURL">
         <v-list-item-icon>
           <v-icon>mdi-file-document-multiple</v-icon>
         </v-list-item-icon>
 
         <v-list-item-content>
-          <v-list-item-title v-text="$('documentation')" />
+          <v-list-item-title v-text="$t('documentation')" />
         </v-list-item-content>
       </v-list-item>
 
-       <v-list-item link :href="mailURL">
-        <v-list-item-icon>
-          <v-icon>mdi-email</v-icon>
-        </v-list-item-icon>
+      <v-list-item
+        v-for="application in applications"
+        :key="application.name"
+        :href="application.link"
+        link
+      >
+        <img
+          style="max-width: 30px"
+          :src="require(`../../../public/images/${application.pathImage}`)"
+          class="mr-6"
+          :alt="`${application.name}-img`"
+        />
 
         <v-list-item-content>
-          <v-list-item-title> Mail </v-list-item-title>
+          <v-list-item-title v-text="application.name" />
         </v-list-item-content>
       </v-list-item>
 
-       <v-list-item link :href="githubURL">
+      <v-list-item
+        v-for="socialNetwork in socialNetworks"
+        :key="socialNetwork.name"
+        :href="socialNetwork.link"
+        link
+      >
         <v-list-item-icon>
-          <v-icon>mdi-github</v-icon>
+          <v-icon> {{ socialNetwork.icon }} </v-icon>
         </v-list-item-icon>
-
         <v-list-item-content>
-          <v-list-item-title> Github </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-       <v-list-item link :href="twitterURL">
-        <v-list-item-icon>
-          <v-icon>mdi-twitter</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title> Twitter </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-       <v-list-item link :href="youtubeURL">
-        <v-list-item-icon>
-          <v-icon>mdi-youtube</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title> Youtube </v-list-item-title>
+          <v-list-item-title v-text="socialNetwork.name" />
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -123,27 +69,20 @@
       prepend-icon="mdi-translate"
     >
       <template #activator>
-        <v-list-item-title
-          class="body-2"
-          v-text="$t('language')"
-        />
+        <v-list-item-title class="body-2" v-text="$t('language')" />
       </template>
 
       <v-list-item
-        @click="$i18n.locale = 'fr'"
-      >
-        <v-list-item-title class="body-2" v-text="$t('french')"/>
+        v-for="lang in langs"
+        :key="lang.locale"
+        @click="$i18n.locale = lang.locale">
+        <v-list-item-title class="body-2" v-text="$t(lang.language)" />
         <v-list-item-icon>
-          <img width="24" :src="require('../../../public/images/lang/fr.png')" alt="fr">
-        </v-list-item-icon>
-      </v-list-item>
-
-      <v-list-item
-        @click="$i18n.locale = 'en'"
-      >
-        <v-list-item-title class="body-2" v-text="$t('english')"/>
-        <v-list-item-icon>
-          <img width="24" :src="require('../../../public/images/lang/en.png')" alt="en">
+          <img
+            width="24"
+            :src="require(`../../../public/images/lang/${lang.locale}.png`)"
+            :alt="lang.locale"
+          />
         </v-list-item-icon>
       </v-list-item>
     </v-list-group>
@@ -156,18 +95,82 @@ export default {
   data() {
     return {
       appVersion: '1.0.0',
-
-      blogURL: 'https://blog.ezpaarse.org/',
-      analogistURL: 'http://analyses.ezpaarse.org/',
-      ezpaarseDemoURL: 'http://demo.ezpaarse.org/',
-      ezmesureURL: 'https://ezmesure.couperin.org/',
-      bibliomapURL: 'https://bibliomap.inist.fr/',
       ezpaarseDocURL: 'https://ezpaarse-project.github.io/ezpaarse/',
-
-      mailURL: 'mailto:ezteam@couperin.org',
-      githubURL: 'https://github.com/ezpaarse-project',
-      twitterURL: 'https://twitter.com/ezpaarse',
-      youtubeURL: 'https://www.youtube.com/channel/UCcR-0UE9WjYiwS4fMG2T4tQ',
+      blogURL: 'https://blog.ezpaarse.org/',
+      applications: [
+        {
+          name: 'ezMESURE',
+          pathImage: 'ezmesure-logo.svg',
+          link: 'https://ezmesure.couperin.org/',
+        },
+        {
+          name: 'ezCOUNTER',
+          pathImage: 'ezcounter-logo.png',
+          link: '',
+        },
+        {
+          name: 'ezREEPORT',
+          pathImage: 'ezmesure-logo.svg',
+          link: '',
+        },
+        {
+          name: 'bibliomap',
+          pathImage: 'bibliomap-logo.svg',
+          link: 'https://bibliomap.inist.fr/',
+        },
+        {
+          name: 'ezPAARSE',
+          pathImage: 'ezpaarse-logo.png',
+          link: 'http://demo.ezpaarse.org/',
+        },
+        {
+          name: 'analogist',
+          pathImage: 'analogist-logo.png',
+          link: 'http://analyses.ezpaarse.org/',
+        },
+        {
+          name: 'ezUNPAYWALL',
+          pathImage: 'circle.png',
+          link: 'https://unpaywall.inist.fr/',
+        },
+        {
+          name: 'readmetrics',
+          pathImage: 'readmetrics-logo.png',
+          link: '',
+        },
+      ],
+      socialNetworks: [
+        {
+          name: 'Mail',
+          icon: 'mdi-email',
+          link: 'mailto:ezteam@couperin.org',
+        },
+        {
+          name: 'Github',
+          icon: 'mdi-github',
+          link: 'https://github.com/ezpaarse-project',
+        },
+        {
+          name: 'Twitter',
+          icon: 'mdi-twitter',
+          link: 'https://twitter.com/ezpaarse',
+        },
+        {
+          name: 'Youtube',
+          icon: 'mdi-youtube',
+          link: 'https://www.youtube.com/channel/UCcR-0UE9WjYiwS4fMG2T4tQ',
+        },
+      ],
+      langs: [
+        {
+          locale: 'fr',
+          language: 'french',
+        },
+        {
+          locale: 'en',
+          language: 'english',
+        },
+      ],
     };
   },
   computed: {
