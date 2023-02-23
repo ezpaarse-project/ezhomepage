a59585a09eb5c447f26acd59fb1b652da4e331f3
felixleo22 <leofelixpro22@gmail.com>
2023-02-23 15:09:33 +0100

    feat: use gif for bibliomap

--- src/components/application/Bibliomap.vue
+++ src/components/application/Bibliomap.vue
@@ -7,22 +7,30 @@
             <h1 class="mx-2">Bibliomap</h1>
           </v-col>
           <v-col cols="12">
-            <p class="ma-2 text text-center text-justify">{{ $t("bibliomap") }}</p>
+            <p class="ma-2 text text-center text-justify">
+              {{ $t("bibliomap") }}
+            </p>
           </v-col>
           <v-col cols="12">
-            <a class="mx-2 text text-center text-justify" :href="link" v-text="$t('learnMore')" />
+            <a
+              class="mx-2 text text-center text-justify"
+              :href="link"
+              v-text="$t('learnMore')"
+            />
           </v-col>
         </v-row>
       </v-col>
       <v-col cols="12" lg="6">
         <v-row>
           <v-card width="100%" justify="center" align="center">
-            <iframe
-              src="https://bibliomap.inist.fr/?d=false&m=false&z=5"
-              width="100%"
-              height="444"
-              title="Bibliomap"
-              style="border: 0;"
+            <video
+              :src="require('../../../public/images/bibliomap-demo.webm')"
+              type="video/webm"
+              class="ma-2 mx-auto"
+              style="max-width: 100%"
+              muted
+              loop
+              autoplay
             />
             <p class="ma-2">{{ $t("bibliomapTitle") }}</p>
           </v-card>
@@ -36,10 +44,10 @@
 export default {
   computed: {
     link() {
-      if (this.$i18n.locale === 'fr') {
-        return 'https://bibliomap.inist.fr/';
+      if (this.$i18n.locale === "fr") {
+        return "https://bibliomap.inist.fr/";
       }
-      return 'https://bibliomap.inist.fr?lang=en';
+      return "https://bibliomap.inist.fr?lang=en";
     },
   },
 };
