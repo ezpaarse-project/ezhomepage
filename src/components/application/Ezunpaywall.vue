2b2abfc81309e5b59ad6f91f32858449e0b463d6
felixleo22 <leofelixpro22@gmail.com>
2023-04-07 08:44:40 +0200

    feat: add unpaywall logo

--- src/components/application/Ezunpaywall.vue
+++ src/components/application/Ezunpaywall.vue
@@ -13,7 +13,19 @@
       <v-col cols="12" lg="6" order="1" order-lg="2">
         <v-row>
           <v-col cols="12" class="justify-center justify-lg-start d-flex">
-            <h1 class="mx-2">ezUNPAYWALL</h1>
+            <v-list-item>
+              <v-list-item-avatar tile="false">
+                <v-img
+                  style="max-width: 35px"
+                  :src="require('../../../public/images/unpaywall-logo.svg')"
+                  class="ma-2 mx-auto"
+                  alt="ezunpaywall-screenshot"
+                />
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <h1> ezUNPAYWALL </h1>
+              </v-list-item-content>
+            </v-list-item>
           </v-col>
           <v-col cols="12">
             <p class="mx-2 text text-center text-justify">
