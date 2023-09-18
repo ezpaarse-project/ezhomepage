5185fd6607ea3cdf3a826735ce675635c6de5954
felixleo22 <leofelixpro22@gmail.com>
2023-09-18 10:57:40 +0200

    fix: update default lang

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -2,7 +2,7 @@ import { createI18n } from "vue-i18n";
 import messages from '@intlify/unplugin-vue-i18n/messages'
 
 export default createI18n({
-  locale: (import.meta.env.VITE_DEFAULT_LOCALE || 'EN').toLowerCase(), 
+  locale: (import.meta.env.VITE_DEFAULT_LOCALE || 'FR').toLowerCase(), 
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
   legacy: false,
   messages
