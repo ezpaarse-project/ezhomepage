b1c8ab9a26143153ecb1ebffb0d2a120f384ff28
felixleo22 <leofelixpro22@gmail.com>
2024-10-23 14:53:01 +0200

    feat: use lang of navigator

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,9 +1,17 @@
 import { createI18n } from "vue-i18n";
 import messages from '@intlify/unplugin-vue-i18n/messages'
 
+function getBrowserLocale() {
+  const navigatorLang = navigator.languages ? navigator.languages[0] : navigator.language
+  console.log(navigatorLang);
+  return navigatorLang ? navigatorLang.split('-')[0].toLowerCase() : null
+}
+
+const locale = getBrowserLocale()
+
 export default createI18n({
-  locale: (import.meta.env.VITE_DEFAULT_LOCALE || 'FR').toLowerCase(), 
-  fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
+  locale, 
+  fallbackLocale: 'en',
   legacy: false,
   messages
 })
