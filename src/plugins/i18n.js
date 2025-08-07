eb3f5797366b42371d61c2f262d6a5c345cb5fbe
felixleo22 <leofelixpro22@gmail.com>
2025-08-07 08:26:14 +0200

    fix: remove console.log

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,7 +3,6 @@ import messages from '@intlify/unplugin-vue-i18n/messages'
 
 function getBrowserLocale() {
   const navigatorLang = navigator.languages ? navigator.languages[0] : navigator.language
-  console.log(navigatorLang);
   return navigatorLang ? navigatorLang.split('-')[0].toLowerCase() : null
 }
 
